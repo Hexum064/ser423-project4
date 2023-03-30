1d14d3549be7d8ebe801fe1a3d701c2545b1e906
BrandenBoucher <bgbouche@asu.edu>
2023-03-29 22:50:23 -0700

    exercise 4

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
 
 import React, { Component } from 'react';
-import {Picker} from '@react-native-picker/picker'; // You'll need this for the exercise
+import { Picker } from '@react-native-picker/picker'; // You'll need this for the exercise
 import {
   Alert,
-  AsyncStorage,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
+  OptionBox
 } from 'react-native';
 
-const key = '@MyApp:key';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+const key1 = '@MyApp:option1key';
+const key2 = '@MyApp:option2key';
+const key3 = '@MyApp:option3key';
+
+const pickerOptions = ["1 Star", "2 Stars", "3 Stars", "4 Stars", "5 Stars"];
 
 export default class App extends Component {
   state = {
-    text: '',
-    storedValue: '',
+    option1: '',
+    option2: '',
+    option3: ''
   };
 
   componentDidMount() {
@@ -25,20 +33,27 @@ export default class App extends Component {
 
   onLoad = async () => {
     try {
-      const storedValue = await AsyncStorage.getItem(key);
-      this.setState({ storedValue });
+      const option1 = (await AsyncStorage.getItem(key1)) ?? 'na';
+      const option2 = (await AsyncStorage.getItem(key2)) ?? 'na';
+      const option3 = (await AsyncStorage.getItem(key3)) ?? 'na';
+
+      this.setState({ option1, option2, option3 });
     } catch (error) {
+      console.log(error);
       Alert.alert('Error', 'There was an error while loading the data');
     }
   }
 
   onSave = async () => {
-    const { text } = this.state;
     try {
-      await AsyncStorage.setItem(key, text);
+      await AsyncStorage.setItem(key1, this.state.option1);
+      await AsyncStorage.setItem(key2, this.state.option2);
+      await AsyncStorage.setItem(key3, this.state.option3);
       Alert.alert('Saved', 'Successfully saved on device');
     } catch (error) {
+      console.log(error);
       Alert.alert('Error', 'There was an error while saving the data');
+
     }
   }
 
@@ -47,19 +62,35 @@ export default class App extends Component {
   }
 
   render() {
-    const { storedValue } = this.state;
-    const { text } = this.state;
+
 
     return (
       <View style={styles.container}>
-        <Text style={styles.preview}>{storedValue}</Text>
-        <View>
-          <TextInput
-            style={styles.input}
-            onChangeText={this.onChange}
-            value={text}
-            placeholder="Type something here..."
-          />
+        <Text style={styles.preview}>Option 1: {this.state.option1}, Option 2: {this.state.option2}, Option 3: {this.state.option3}</Text>
+        <View style={styles.input}>
+          <View style={{ flexDirection: 'row' }}>
+            <Picker style={{ flex: 1 }} mode='dropdown'
+              selectedValue={this.state.option1}
+              onValueChange={(value) => { this.setState({ option1: value }); }}>
+              {pickerOptions.map((option) => {
+                return <Picker.Item key={option} label={option} value={option} />;
+              })}
+            </Picker>
+            <Picker style={{ flex: 1 }}
+              selectedValue={this.state.option2}
+              onValueChange={(value) => { this.setState({ option2: value }); }}>
+              {pickerOptions.map((option) => {
+                return <Picker.Item key={option} label={option} value={option} />;
+              })}
+            </Picker>
+            <Picker style={{ flex: 1 }}
+              selectedValue={this.state.option3}
+              onValueChange={(value) => { this.setState({ option3: value }); }}>
+              {pickerOptions.map((option) => {
+                return <Picker.Item key={option} label={option} value={option} />;
+              })}
+            </Picker>
+          </View>
           <TouchableOpacity onPress={this.onSave} style=
             {styles.button}>
             <Text>Save locally</Text>
@@ -73,6 +104,7 @@ export default class App extends Component {
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -91,8 +123,7 @@ const styles = StyleSheet.create({
   }, input: {
     backgroundColor: '#ecf0f1',
     borderRadius: 3,
-    width: 300,
-    height: 40,
+    width: 400,
     padding: 5,
   },
   button: {
